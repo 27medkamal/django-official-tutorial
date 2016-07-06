e81c4acb3663d5932bd63568092f5bea8637ca74
Ahmed Kamal <ahmed_kamal_abulinain@yahoo.com>
2016-07-06 14:58:49 +1000

    Writing your first Django app, part 5

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -11,13 +12,21 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
 
     def get_queryset(self):
-        """return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """return the last five published questions (not including theose set to be published in the future)."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return self.model.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):

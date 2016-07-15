1fd83bf2196a63b5021b600c2830a3bba92c130a
Ahmed Kamal <ahmed_kamal_abulinain@yahoo.com>
2016-07-15 15:19:00 +1000

    fix typo

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
 
     # def get_queryset(self):
-    #     """return the last five published questions (not including theose set to be published in the future)."""
+    #     """return the last five published questions (not including those set to be published in the future)."""
     #     return Question.objects.filter(
     #         pub_date__lte=timezone.now()
     #     ).order_by('-pub_date')[:5]
